c0d023b23e5edbaa6dd7caad5ac46f1f70b47b9a
AllyW <liwang1025@gmail.com>
2025-02-23 18:21:54 +0800

    try search import tsp file namespace

--- src/aaz_dev/swagger/model/specs/_typespec_helper.py
+++ src/aaz_dev/swagger/model/specs/_typespec_helper.py
@@ -47,6 +47,7 @@ class TypeSpecHelper:
             lines = f.readlines()
         is_mgmt_plane = False
         namespace = None
+        imported_tsps = []
         for line in lines:
             if line.startswith("@armProviderNamespace"):
                 is_mgmt_plane = True
@@ -55,7 +56,36 @@ class TypeSpecHelper:
                 namespace = re.match(r"^namespace\s+([A-Za-z0-9.]+)", line).group(1)
                 # armProviderNamespace will always be appeared before namespace
                 break
+            import_tsp = re.search(r'import\s+["\']([^"\']+\.tsp)["\']', line)
+            if import_tsp:
+                imported_tsps.append(import_tsp.group(1))
+
         if namespace is None:
-            # logger.warning("Failed to parse main tsp file: %s namespace is not exist.", path)
-            return None, None
+            logger.info("Failed to parse main tsp file: %s namespace is not exist.", path)
+            if not imported_tsps:
+                return None, None
+            logger.info("Try to parse namespace from the imports files %s from main tsp file: %s.", imported_tsps, path)
+            return cls._parse_main_tsp_imports(path, imported_tsps)
         return namespace, is_mgmt_plane
+
+    @classmethod
+    def _parse_main_tsp_imports(cls, main_path, imported_tsps):
+        base_dir = os.path.dirname(main_path)
+        for check_file in imported_tsps:
+            check_file_path = os.path.abspath(os.path.join(base_dir, check_file))
+            is_mgmt_plane = False
+            namespace = None
+            with open(check_file_path, 'r', encoding='utf-8') as f:
+                lines = f.readlines()
+            for line in lines:
+                if line.startswith("@armProviderNamespace"):
+                    is_mgmt_plane = True
+                if line.startswith("namespace "):
+                    namespace = re.match(r"^namespace\s+([A-Za-z0-9.]+)", line).group(1)
+                    # armProviderNamespace will always be appeared before namespace
+                    break
+            if namespace is not None:
+                logger.info("Find namespace %s from the imports file %s.", namespace, check_file)
+                return namespace, is_mgmt_plane
+        return None, None
+
